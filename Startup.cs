1c94cdf0b6cd3f8634ca7f518c3ee2e30e0cf554
Jessica Wailes <jess.m.wailes@gmail.com>
2019-04-23 07:30:33 -0700

    Add files via upload
    Madness Incarnate

---
+++ Startup.cs
@@ -0,0 +1,72 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using Microsoft.ApplicationInsights;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Bot.Builder;
+using Microsoft.Bot.Builder.Integration.ApplicationInsights.Core;
+using Microsoft.Bot.Builder.Integration.AspNet.Core;
+using Microsoft.Bot.Connector.Authentication;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.BotBuilderSamples.Telemetry;
+
+namespace Microsoft.BotBuilderSamples
+{
+    public class Startup
+    {
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            
+            // Create the credential provider to be used with the Bot Framework Adapter.
+            services.AddSingleton<ICredentialProvider, ConfigurationCredentialProvider>();
+
+            // Create the Bot Framework Adapter with error handling enabled. 
+            services.AddSingleton<IBotFrameworkHttpAdapter, AdapterWithErrorHandler>();
+
+            // Create the bot services (LUIS, QnA) as a singleton.
+            services.AddSingleton<IBotServices, BotServices>();
+
+            #region newSingletons
+            // Create the storage we'll be using for the User and Conversation State. (Memory is great for testing purposes)
+            services.AddSingleton<IStorage, MemoryStorage>();
+
+            // Create the User state.
+            services.AddSingleton<UserState>();
+
+            // Create Telemetry Client
+            services.AddSingleton<TelemetryLuisRecognizer>();
+
+            // Create the Telemetry Middleware that will be added to the middlware pipeline in the AdapterWithErrorHandler.
+            services.AddSingleton<IMiddleware, TelemetryLoggerMiddleware>();
+            
+
+            #endregion
+
+            // Create the bot as a transient.
+            services.AddTransient<IBot, LuisAppInsightsBot>();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseHsts();
+            }
+
+            app.UseDefaultFiles();
+            app.UseStaticFiles();
+
+            //app.UseHttpsRedirection();
+            app.UseMvc();
+        }
+    }
+}
